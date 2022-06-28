52be99d05b0a661c387972c72d153db90de281a6
nazufel <nazufel@gmail.com>
2022-06-28 16:32:22 -0400

    first deploy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,15 @@
-<div class="hero min-h-screen bg-base-200">
-    <div class="hero-content text-center">
-      <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Hello there</h1>
-        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <button class="btn btn-secondary">Get Started</button>
-      </div>
+
+<html lang="en">
+    <div class="hero">
+        <div class="hero min-h-screen bg-base-200">
+            <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-5xl text-secondary font-bold">Hello There</h1>
+                <p class="py-6">Awesome stories are coming.</p>
+                <!-- <p class="py-6">Awesome stories are coming. Please up to be notified.</p> -->
+                <!-- <button class="btn btn-primary">Sign Up</button> -->
+            </div>
+            </div>
+        </div>
     </div>
-  </div>
+</html>
