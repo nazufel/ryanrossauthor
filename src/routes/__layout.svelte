6f2ff32e2c8fbc70cbb772a76919aa5fe76e3333
nazufel <nazufel@gmail.com>
2022-06-28 22:54:49 -0400

    have privacy policy

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,11 @@
   import "../app.css";
 </script>
 
+
 <slot />
+
+<footer class="footer bg-secondary footer-center p-4 text-base-content">
+    <div>
+        <p class="text-base-300">{'Copyright © '} {new Date().getFullYear()} - Ryan Ross | <a class="link link-neutral" href="/privacy">Privacy Policy</a></p>
+    </div>
+</footer>
---
+++ src/routes/privacy.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+    <title>Ryan Ross | Privacy</title>
+</svelte:head>
+
+<html lang="en">
+    <div class="h-screen">
+        <p class="m-10">This site does not collect visitor data.</p>
+        <a class="btn btn-primary m-10" href="/">Back</a>
+    </div>
+</html>
